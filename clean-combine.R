12bc5ed58d439e5c432adefcd6d049956a6f8d5b
jmk <BobHarper1@users.noreply.github.com>
2016-03-07 15:29:35 +0000

    Update clean-combine.R
    using ckan api to get the data package json. much more reliable

--- clean-combine.R
+++ clean-combine.R
@@ -1,8 +1,10 @@
-library(httr)
+library(jsonlite)
 library(plyr)
-library(XML)
 
-range <- 1:2 ## last x months to work with. 1 = most recent month on https://www.opendatani.gov.uk/dataset/gp-prescribing-data
+
+range <- 1:6 ## x months to work with. Can change start and end of range. 
+             ## 1 = most recent month on https://www.opendatani.gov.uk/dataset/gp-prescribing-data
+             ## So be aware you're working backwards
 
 if(dir.exists('~/GP Prescribing Data') != TRUE){
   dir.create('~/GP Prescribing Data')
@@ -12,45 +14,43 @@ setwd("~/GP Prescribing Data/")
 wd <- getwd()
 
 newCols<-as.character(c("id","practice","year","month","vtm_nm","vmp_nm","amp_nm",
-                      "presentation","strength","total.items","total.quantity",
-                      "gross.cost","actual.cost","bnf.code","bnf.chapter",
-                      "bnf.section","bnf.paragraph","bnf.sub.paragraph"))
+                        "presentation","strength","total.items","total.quantity",
+                        "gross.cost","actual.cost","bnf.code","bnf.chapter",
+                        "bnf.section","bnf.paragraph","bnf.sub.paragraph"))
 
 cClasses<-c("integer","integer","integer","factor","factor","factor",
             "factor","factor","integer","integer",
             "numeric","numeric","factor","integer",
             "integer","integer","integer")
 
-# Download the html for the dataset webpage and create lists of all available resources
-webpage <- content(GET("http://www.opendatani.gov.uk/dataset/gp-prescribing-data"), as="text")
-htmldoc <- htmlParse(webpage)
-dataids <- xpathSApply(htmldoc, "//li[@class='resource-item']", xmlGetAttr,"data-id")
-titles <- xpathSApply(htmldoc, "//a[@class='heading']", xmlGetAttr, "title")
-df <- data.frame(dataids,titles)
+## Use the OpenDataNI package API for the GP prescribing dataset (providing the id doesn't change)
+datapackage <- fromJSON('https://www.opendatani.gov.uk/api/3/action/package_show?id=a7b76920-bc0a-48fd-9abf-dc5ad0999886')
+resources <- datapackage$result$resources
 
 ## Create a list of the files that we actually want (the range)
 filenames <- c()
 for (i in range){
-  name <- paste(df[2][i,],'.csv',sep='')
+  name <- paste(resources$name[i],'.csv',sep='')
   filenames <- append(filenames,as.character(name))
 }
 
+print(paste("Downloading and combining files between",resources$name[max(range)],"and",resources$name[min(range)]))
+
 ## Download the actual files
-datastoreUrl <- "https://www.opendatani.gov.uk/datastore/dump/"
 for (i in range) {
-  download.file(paste(datastoreUrl,df[1][i,],sep=''), paste(df[2][i,],'.csv',sep=''),method='libcurl')
+  download.file(resources$url[i], paste(resources$name[i],".csv",sep=''),method='libcurl')
 }
 
 combined <- c()
 
 ## Then, we can stitch the files together (it doesn't matter on the order, the month number is a field):
 for (i in filenames){
-    temp_dataset <- read.csv(i, header=TRUE, na.strings="-")
-    combined <- rbind.fill(combined, temp_dataset)
-    rm(temp_dataset)
-  }
+  temp_dataset <- read.csv(i, header=TRUE, na.strings="-")
+  combined <- rbind.fill(combined, temp_dataset)
+  rm(temp_dataset)
+}
 
-colnames(combined) <- newCols
+colnames(combined) <- tolower(colnames(combined))
 
 ## Now, let's sort by the medicine type using the British National Formulary (BNF) coding system, then month:
 attach(combined)
@@ -59,5 +59,5 @@ combined <- combined[order(bnf.chapter, bnf.section, bnf.paragraph,
 detach(combined)
 
 ## Write the combined file to disk
-write.table(combined, row.names=FALSE, paste("combined-",range,"-mostrecentmonths-",Sys.Date(),".csv",sep=""))
-print("Done")
+write.csv(combined, row.names=FALSE, paste("combined-prescribing-dataset-",Sys.Date(),".csv",sep=""))
+print(paste("Created file for range between",min(filenames),"and",max(filenames)))
